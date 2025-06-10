2f32169b586716611f4c4f0e9dbe5a7603397f38
carlportz <derlasse25@gmail.com>
2025-06-10 20:37:13 +0200

    Add DOLFINx and PyTorch installation scripts and configuration for GPU support in dev container

---
+++ test_installation.py
@@ -0,0 +1,9 @@
+import dolfinx
+print(f"DOLFINx version: {dolfinx.__version__} based on GIT commit: {dolfinx.git_commit_hash} of https://github.com/FEniCS/dolfinx/")
+
+import torch
+print(f"PyTorch version: {torch.__version__}")
+print(f"GPU available: {torch.cuda.is_available()}")
+print(f"Number of GPUs: {torch.cuda.device_count()}")
+print(f"Current device: {torch.cuda.current_device()}")
+print(f"Device name: {torch.cuda.get_device_name(torch.cuda.current_device())}")
